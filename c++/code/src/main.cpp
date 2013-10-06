102d7063de078e29d78a3e2543169fa969c96618
Greg Korthuis <gpkort@gmail.com>
2013-10-06 08:34:03 -0600

    new main.cpp

---
+++ c++/code/src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jack
+ *
+ * Created on October 6, 2013, 8:28 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
